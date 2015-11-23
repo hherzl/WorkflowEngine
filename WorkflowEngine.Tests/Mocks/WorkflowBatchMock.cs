5ac7e6175c4a5d2a18b9e6b8031f6b2e7ceed9ec
hans.hernandez <hans.hernandez@m-banco.com>
2015-11-23 11:17:46 -0600

    Addition of tests for workflow model

---
+++ WorkflowEngine.Tests/Mocks/WorkflowBatchMock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using WorkflowEngine.Model;
+
+namespace WorkflowEngine.Tests.Mocks
+{
+    public static class WorkflowBatchMock
+    {
+        public static WorkflowBatch GetRegisterCustomerWorkflowBatch()
+        {
+            var workflowBatch = new WorkflowBatch();
+
+            workflowBatch.Constants = new List<WorkflowConstant>()
+            {
+                new WorkflowConstant
+                {
+                    Name = "CountryName",
+                    Value = "UK"
+                }
+            };
+
+            workflowBatch.Workflows = new List<Workflow>()
+            {
+                new Workflow
+                {
+                    Name = "Register customer",
+                    Description = "Register new customer",
+                    Tasks = new List<WorkflowTask>()
+                    {
+                        new WorkflowTask
+                        {
+                            Name = "Add customer's data",
+                            Description = "Add information for customer's fields",
+                            Parameters = new List<WorkflowParameter>()
+                            {
+                                new WorkflowParameter { Key = "CompanyName", Value = "Acme" },
+                                new WorkflowParameter { Key = "ContactName", Value = "John Doe" },
+                                new WorkflowParameter { Key = "ContactTitle", Value = "Mr." },
+                                new WorkflowParameter { Key = "Country", Constant = "CountryName" },
+                            }
+                        }
+                    }
+                }
+            };
+
+            return workflowBatch;
+        }
+    }
+}
---
+++ WorkflowEngine.Tests/WorkflowUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkflowEngine.Model.Execution;
+using WorkflowEngine.Model.Serialization;
+using WorkflowEngine.Tests.Mocks;
+
+namespace WorkflowEngine.Tests
+{
+    [TestClass]
+    public class WorkflowUnitTest
+    {
+        String AppDirectory
+        {
+            get
+            {
+                return String.Format(@"{0}\Xml\RegisterCustomer.xml", Environment.CurrentDirectory.Replace(@"\bin\Debug", String.Empty));
+            }
+        }
+
+        [TestMethod]
+        public void SerializeRegisterCustomerWorkflow()
+        {
+            var workflowBatch = WorkflowBatchMock.GetRegisterCustomerWorkflowBatch();
+
+            var serializer = new XmlSerializerImplementation() as ISerializer;
+
+            var xml = serializer.Serialize(workflowBatch);
+
+            Console.WriteLine(xml);
+
+            // <?xml-stylesheet type='text/xsl" href="RegisterCustomer.Results.xsl" ?>
+            File.WriteAllText(AppDirectory, xml);
+        }
+
+        [TestMethod]
+        public void RunBasicWorkflow()
+        {
+            var runner = new WorkflowRunner(WorkflowBatchMock.GetRegisterCustomerWorkflowBatch());
+
+            var executionSummary = runner.Execute();
+
+            var serializer = new XmlSerializerImplementation() as ISerializer;
+
+            var xml = serializer.Serialize(executionSummary);
+
+            Console.WriteLine(xml);
+
+            File.WriteAllText(String.Format(@"{0}\ExecutionResults\RegisterCustomer.Results.xml", Environment.CurrentDirectory.Replace(@"\bin\Debug", String.Empty)), xml);
+        }
+    }
+}
