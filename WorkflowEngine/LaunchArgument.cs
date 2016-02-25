21c4e0b934c202b03cc11dca72936142b09fbe23
hans.hernandez <hans.hernandez@m-banco.com>
2016-02-25 11:32:54 -0600

    Code improvement: addition of command line package for parsing

--- WorkflowEngine/LaunchArgument.cs
+++ WorkflowEngine/LaunchArgument.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CommandLine;
 
 namespace WorkflowEngine
 {
@@ -9,22 +10,31 @@ namespace WorkflowEngine
 
         }
 
+        [Option("ui", HelpText = "User Interface")]
         public String UI { get; set; }
 
+        [Option("domain", HelpText = "Domain")]
         public String Domain { get; set; }
 
+        [Option("user", HelpText = "User")]
         public String User { get; set; }
 
+        [Option("password", HelpText = "Password")]
         public String Password { get; set; }
 
+        [Option("sd", HelpText = "Source Directory")]
         public String SourceDirectory { get; set; }
 
+        [Option("sf", HelpText = "Source File")]
         public String SourceFile { get; set; }
 
+        [Option("od", HelpText = "Output Directory")]
         public String OutputDirectory { get; set; }
 
+        [Option("of", HelpText = "Output File")]
         public String OutputFile { get; set; }
 
+        [Option("off", HelpText = "Output File Format")]
         public String OutputFileFormat { get; set; }
     }
 }
