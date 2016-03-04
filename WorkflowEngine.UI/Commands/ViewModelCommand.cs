640cb664811320f5422ff4bc0b2ec26735470687
hherzl <hans.herzl@live.com>
2016-03-04 07:59:18 -0600

    Code improvement/; addition of fields for ui

---
+++ WorkflowEngine.UI/Commands/ViewModelCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WorkflowEngine.UI.Commands
+{
+    public class ViewModelCommand : ICommand
+    {
+        private Action m_action;
+        private Boolean m_canExecute;
+
+        public ViewModelCommand(Action action, Boolean canExecute)
+        {
+            m_action = action;
+            m_canExecute = canExecute;
+        }
+
+        public Boolean CanExecute(Object parameter)
+        {
+            return m_canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(Object parameter)
+        {
+            m_action();
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
+}
--- WorkflowEngine.UI/ViewModels/LayoutViewModel.cs
+++ WorkflowEngine.UI/ViewModels/LayoutViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Win32;
+using WorkflowEngine.UI.Commands;
 
 namespace WorkflowEngine.UI.ViewModels
 {
@@ -13,6 +16,7 @@ namespace WorkflowEngine.UI.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         private String m_title;
+        private String m_fileName;
 
         public String Title
         {
@@ -31,6 +35,51 @@ namespace WorkflowEngine.UI.ViewModels
                         PropertyChanged(this, new PropertyChangedEventArgs("Title"));
                     }
                 }
+            }
+        }
+
+        public String FileName
+        {
+            get
+            {
+                return m_fileName;
+            }
+            set
+            {
+                if (m_fileName != value)
+                {
+                    m_fileName = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("FileName"));
+                    }
+                }
+            }
+        }
+
+        private ICommand m_loadFileCommand;
+
+        public ICommand LoadFileCommand
+        {
+            get
+            {
+                return m_loadFileCommand ?? (m_loadFileCommand = new ViewModelCommand(LoadFileAction, true));
+            }
+        }
+
+        public void LoadFileAction()
+        {
+            var dialog = new OpenFileDialog()
+            {
+                DefaultExt = ".xml"
+            };
+
+            var result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                FileName = dialog.FileName;
             }
         }
     }
